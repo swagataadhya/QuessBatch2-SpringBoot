702cc7e124afc471d472c36c21af05ab7a4e775b
Swagata Adhya <103314170+swagataadhya@users.noreply.github.com>
2022-08-16 13:19:16 +0530

    [Spring Cloud Config]

---
+++ SpringCloudConfig/src/test/java/com/example/Quess/SpringCloudConfig/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quess.SpringCloudConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
